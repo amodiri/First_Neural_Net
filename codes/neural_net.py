7a6931d0e9c2a2d63663ad1b7bcd6c0998dd90dc
amodiri <modiri.arghavan@queensu.ca>
2017-12-02 09:13:31 -0500

    Added folder to manage codes

---
+++ codes/neural_net.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class neural_network:
+	def init_with_zeros(self, size):
+		self.w = np.zeros((size.shape[0], size.shape[1]))
+		self.b = np.zeros((size.shape[1]))
+	def init_with_rands(self,size):
+	def init_better(self,size):
+	def propagate(self):
+	def SGD_optimization(self):
+	def predict(self):
+
+
