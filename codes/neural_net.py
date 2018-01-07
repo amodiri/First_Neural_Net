75c9b4fd2f4ae6dcdf5c7256545d6abea7f60aff
amodiri <modiri.arghavan@queensu.ca>
2018-01-06 21:37:22 -0500

    Added docs

--- codes/neural_net.py
+++ codes/neural_net.py
@@ -1,3 +1,10 @@
+"""
+This module implements neural network models with arbitrary number of layers
+and hidden units. The size of network should be defined when the object is
+created. Forward propagation uses Gradient descent algorithm and there are
+several options available for initializing the Neural Net parameters.
+"""
+
 import numpy as np
 import mnist_loader
 import sys
@@ -5,32 +12,69 @@ import numpy
 from sklearn.preprocessing import OneHotEncoder
 numpy.set_printoptions(threshold=sys.maxsize)
 
-DEBUG=True
+
 class Neural_Net(object):
 
-    def log(self, msg):
-        if (DEBUG):
-            print(msg)
-
     def __init__(self, nn_dims, init_method='init_with_rands'):
+        """
+        Initializes the class variables and calls the selected initialization
+        method for NN parameters.
+
+        Args:
+            nn_dims (list): size of the neural net
+            init_method (str): method of parameters initialization. If not
+            specified, random initialization will be selected.
+
+        Returns:
+            None
+        """
         self.L = len(nn_dims)
         self.params = {}
         self.activations = {}
         self.Z = {}
-        self.init_with_rands(nn_dims)
 
-    def init_with_zeroBias(self):
-        pass
+        if(init_method == 'init_with_rands'):
+            self.init_with_rands(nn_dims)
+
 
     def init_with_rands(self, nn_dims):
+        """
+        Initializes the parameters of NN randomly.
+
+        Args:
+            nn_dims (list): size of the neural net
+
+        Returns:
+            None
+        """
         for l in range(1,self.L):
             self.params['W'+str(l)] = np.random.randn(nn_dims[l],nn_dims[l-1]) * 0.01
             self.params['b'+str(l)] = np.zeros((nn_dims[l],1))
 
+
     def init_better(self):
+        """
+        Initializes the parameters of NN.
+
+        Args:
+            nn_dims (list): size of the neural net
+
+        Returns:
+            None
+        """
         self.w1 = np.zeros((nh,nx))
 
+
     def forward_propagation(self, X):
+        """
+        Implements forward propagation using gradient descent algorithm.
+
+        Args:
+            X (array): Input of NN
+
+        Returns:
+            None
+        """
         self.activations[0] = X
         L = self.L - 1
         for l in range(1, L):
@@ -42,7 +86,19 @@ class Neural_Net(object):
         temp = np.exp(self.Z[L])
         self.activations[L] = temp / np.sum(temp, axis=0)
 
+
     def backward_propagatation(self, Y_nn, Y):
+        """
+        Implements backward propagation
+
+        Args:
+            Y_nn (array): NN estimated output
+            Y (array): the given output
+
+        Returns:
+            Dictionary containing all gradient descent updates for parameters
+            of NN
+        """
         L = self.L - 1
         grads = {}
         m = Y.shape[1]
@@ -62,6 +118,19 @@ class Neural_Net(object):
 
 
     def nn_training(self, X_train, Y_train, iterations, alpha):
+        """
+        Implements backward propagation
+
+        Args:
+            X_train (array): training set inputs
+            Y_train (array): training set outputs
+            iterations (int): number of iterations for gradient descent
+            alpha (float): learning rate
+
+        Returns:
+            Dictionary containing all gradient descent updates for parameters
+            of NN
+        """
         for i in range(0, iterations):
             self.forward_propagation(X_train)
             cost = self.evaluate(self.activations[self.L-1],Y_train)
@@ -73,16 +142,50 @@ class Neural_Net(object):
 
 
     def evaluate(self, Y_nn, Y):
+        """
+        Computes the cost value for the trained NN
+
+        Args:
+            Y_nn (array): NN estimated output
+            Y (array): the given output
+
+        Returns:
+            value of cost function based on the given output values and their
+            estimations
+        """
         m = Y.shape[1]
         cost = -1*np.sum(np.multiply(Y, np.log(Y_nn))) / m
         return np.squeeze(cost)
 
+
     def update_params(self, grads, alpha):
+        """
+        Updates the parameters of NN based on the learning rate and the update
+        values
+
+        Args:
+            grads (dict): all gradient descent updates for parameters of NN
+            alpha (float): learning rate
+
+        Returns:
+            None
+        """
         for l in range(1, self.L):
             self.params['W'+str(l)] = self.params['W'+str(l)] - alpha*grads['dW'+str(l)]
             self.params['b'+str(l)] = self.params['b'+str(l)] - alpha*grads['db'+str(l)]
 
+
     def predict(self, X_test, Y_test):
+        """
+        Predicts the output based on the given input. Also, calculated the accuracy of the prediction.
+
+        Args:
+            X_test (array): test set inputs
+            Y_test (array): test set outputs
+
+        Returns:
+            An array of the predicted outputs
+        """
         m = X_test.shape[1]
         self.forward_propagation(X_test)
         Y_pred = self.activations[self.L-1]
@@ -92,10 +195,18 @@ class Neural_Net(object):
         print("Accuracy : ", accuracy*100, " %")
         return Y_pred
 
+
     def sigmoid(self, z):
+        """
+        Calculates the value of Sigmoid function for the given input
+        """
         return 1/(1+np.exp(-z))
 
+
     def relu(self,z):
+        """
+        Calculates the value of Relu function for the given input
+        """
         return np.maximum(0,z)
 
 
